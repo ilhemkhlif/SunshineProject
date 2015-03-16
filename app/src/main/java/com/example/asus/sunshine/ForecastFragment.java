6e26a9722bb993ad495d0ffb173296d1b6b0cf12
ilhemkhlif <ilhem.khlif.tn@ieee.org>
2015-03-16 23:27:44 +0100

    Detail Settings and Map

--- app/src/main/java/com/example/asus/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/asus/sunshine/ForecastFragment.java
@@ -1,8 +1,11 @@
 package com.example.asus.sunshine;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,9 +30,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by ASUS on 17/02/2015.
@@ -42,6 +44,7 @@ public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,8 +65,8 @@ public class ForecastFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+
+               updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -73,37 +76,49 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
-        // Now that we have some dummy forecast data, create an ArrayAdapter.
-        // The ArrayAdapter will take data from a source (like our dummy forecast) and
+
+        // The ArrayAdapter will take data from a source and
         // use it to populate the ListView it's attached to.
         mForecastAdapter =
                 new ArrayAdapter<String>(
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_forecast, // The name of the layout ID.
                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        weekForecast);
-
+                        new ArrayList<String>());
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
-        return rootView;
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView <?> adapterView, View view, int position, long l) {
+            String forecast = mForecastAdapter.getItem(position);
+                        //  Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getActivity(), DetailActivity.class)
+                                    .putExtra(Intent.EXTRA_TEXT, forecast);
+                            startActivity(intent);
+        }
+    });
+
+    return rootView;
     }
 
+    private void updateWeather() {
+               FetchWeatherTask weatherTask = new FetchWeatherTask();
+              SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+              String location = prefs.getString(getString(R.string.pref_location_key),
+                              getString(R.string.pref_location_default));
+               weatherTask.execute(location);
+         }
+
+           @Override
+        public void onStart() {
+               super.onStart();
+               updateWeather();
+           }
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
@@ -124,6 +139,22 @@ public class ForecastFragment extends Fragment {
          */
         private String formatHighLows(double high, double low) {
             // For presentation, assume the user doesn't care about tenths of a degree.
+            // Data is fetched in Celsius by default.
+                        // If user prefers to see in Fahrenheit, convert the values here.
+                                // We do this rather than fetching in Fahrenheit so that the user can
+                                       // change this option without us having to re-fetch the data once
+                                                // we start storing the values in a database.
+                                                       SharedPreferences sharedPrefs =
+                                        PreferenceManager.getDefaultSharedPreferences(getActivity());
+                        String unitType = sharedPrefs.getString(
+                                        getString(R.string.pref_units_key),
+                                        getString(R.string.pref_units_metric));
+                                if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                                high = (high * 1.8) + 32;
+                                low = (low * 1.8) + 32;
+                            } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                                Log.d(LOG_TAG, "Unit type not found: " + unitType);
+                            }
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
 
